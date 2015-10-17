70eeac9d07f8afb60707389d2bf11641373b8950
Ramiro Meares <ramiro.meares@ampush.com>
2015-10-17 15:16:13 -0300

    Implemented algorithm

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from random import shuffle
 import argparse
+
+N, S, E, W = 'N', 'S', 'E', 'W'
+DX = {E: 1, W: -1, N: 0, S: 0}
+DY = {E: 0, W: 0, N: -1, S: 1}
+OPPOSITE = {E: W, W: E, N: S, S: N}
 
 
 class Maze:
@@ -15,32 +21,47 @@ class Maze:
         self.container = self.initiate()
 
     def __str__(self):
+        return "Maze H: {} - W: {}".format(self.height, self.width)
+
+    def print_maze(self):
         ascii_maze = " " + "_" * (self.width * 2) + "\n"
         for row in self.container:
             ascii_maze += '|'
             for cell in row:
-                ascii_maze += ' |'
+                ascii_maze += " " if ((cell.carve_direction and S) != 0) else "_"
+                if cell.carve_direction and E != 0:
+                    ascii_maze += " " if ((cell.carve_direction or row[cell.x + 1].carve_direction) and S) != 0 else "_"
+                else:
+                    ascii_maze += "|"
             ascii_maze += '\n'
         ascii_maze += " " + "_" * (self.width * 2) + "\n"
-        return ascii_maze
+        print ascii_maze
 
     def initiate(self):
-        maze = list()
+        m = list()
         for y in range(self.height):
             row = list()
             for x in range(self.width):
                 row.append(Cell(x, y))
-            maze.append(row)
-        return maze
+            m.append(row)
+        return m
 
-    def carve_passage_from(self, cx, cy, grid):
-        pass
+    def carve_passages_from(self, cx, cy):
+        directions = [N, S, E, W]
+        shuffle(directions)
+        for direction in directions:
+            nx = cx + DX[direction]
+            ny = cy + DY[direction]
+            if (0 <= ny < self.height) and (0 <= nx < self.width) and not self.container[ny][nx].carve_direction:
+                self.container[cy][cx].carve_direction.append(direction)
+                self.container[ny][nx].carve_direction.append(OPPOSITE[direction])
+                self.carve_passages_from(nx, ny)
 
 
 class Cell:
     x = 0
     y = 0
-    carve_direction = None
+    carve_direction = list()
 
     def __init__(self, x, y):
         self.x = x
@@ -64,4 +85,5 @@ def parse_arguments():
 if __name__ == '__main__':
     args = parse_arguments()
     maze = Maze(args.height, args.width)
-    print maze
+    maze.carve_passages_from(0, 0)
+    maze.print_maze()
