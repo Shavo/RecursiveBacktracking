c930a683303bdf6208b2e5283687e83157f43c9c
Ramiro Meares <ramiro.meares@ampush.com>
2015-11-14 19:12:46 -0300

    Added WALL_CHAR constant

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from random import shuffle
 import argparse
 
@@ -5,6 +6,8 @@ N, S, E, W = 'N', 'S', 'E', 'W'
 DX = {E: 1, W: -1, N: 0, S: 0}
 DY = {E: 0, W: 0, N: -1, S: 1}
 OPPOSITE = {E: W, W: E, N: S, S: N}
+
+WALL_CHAR = "█"
 
 
 class Maze:
@@ -24,21 +27,21 @@ class Maze:
         return "Maze H: {} - W: {}".format(self.height, self.width)
 
     def print_maze(self):
-        ascii_maze = "#" * (self.width * 2 + 1) + "\n"
+        ascii_maze = "{}".format(WALL_CHAR) * (self.width * 2 + 1) + "\n"
         for row in self.container:
-            ascii_maze += '#'
+            ascii_maze += "{}".format(WALL_CHAR)
             for cell in row:
                 if E in cell.carve_direction:
                     ascii_maze += "  "
                 else:
-                    ascii_maze += " #"
-            ascii_maze += '\n#'
+                    ascii_maze += " {}".format(WALL_CHAR)
+            ascii_maze += "\n{}".format(WALL_CHAR)
             for cell in row:
                 if S in cell.carve_direction:
-                    ascii_maze += " #"
+                    ascii_maze += " {}".format(WALL_CHAR)
                 else:
-                    ascii_maze += "##"
-            ascii_maze += '\n'
+                    ascii_maze += "{}".format(WALL_CHAR) * 2
+            ascii_maze += "\n"
         print ascii_maze
 
     def initiate(self):
@@ -83,9 +86,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--height', '-hh', dest='height', help='Height of the maze',
-                        type=int, required=False, default=5)
+                        type=int, required=False, default=18)
     parser.add_argument('--width', '-ww', dest='width', help='Width of the maze',
-                        type=int, required=False, default=5)
+                        type=int, required=False, default=50)
 
     return parser.parse_args()
 
